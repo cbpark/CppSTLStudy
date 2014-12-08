a48c0663e5328bdcd56574f0bb82d09173f2c7b9
cbpark <cbpark@gmail.com>
2014-12-09 00:51:09 +0900

    Add src/util, .projectile, modify configure.ac, Makefile.am

---
+++ src/util/pair1.cc
@@ -0,0 +1,34 @@
+/* The following code example is taken from the book
+ * "The C++ Standard Library - A Tutorial and Reference, 2nd Edition"
+ * by Nicolai M. Josuttis, Addison-Wesley, 2012
+ *
+ * (C) Copyright Nicolai M. Josuttis 2012.
+ * Permission to copy, use, modify, sell and distribute this software
+ * is granted provided this copyright notice appears in all copies.
+ * This software is provided "as is" without express or implied
+ * warranty, and with no claim as to its suitability for any purpose.
+ */
+#include <iostream>
+#include <tuple>
+#include <utility>
+
+struct Foo {
+    Foo(std::tuple<int, double>) {
+        std::cout << "Foo::Foo(tuple)\n";
+    }
+    template <typename... Args>
+    Foo (Args... args) {
+        std::cout << "Foo::Foo(args...)\n";
+    }
+};
+
+int main() {
+    // create tuple t.
+    std::tuple<int, double> t(1, 2.22);
+
+    // pass the tuple as a whole to the constructor of Foo.
+    std::pair<int, Foo> p1(42, t);
+
+    // pass the elements of the tuple to the constructor of Foo.
+    std::pair<int, Foo> p2(std::piecewise_construct, std::make_tuple(42), t);
+}
---
+++ src/util/tuple1.cc
@@ -0,0 +1,44 @@
+/* The following code example is taken from the book
+ * "The C++ Standard Library - A Tutorial and Reference, 2nd Edition"
+ * by Nicolai M. Josuttis, Addison-Wesley, 2012
+ *
+ * (C) Copyright Nicolai M. Josuttis 2012.
+ * Permission to copy, use, modify, sell and distribute this software
+ * is granted provided this copyright notice appears in all copies.
+ * This software is provided "as is" without express or implied
+ * warranty, and with no claim as to its suitability for any purpose.
+ */
+#include <complex>
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <utility>
+
+int main() {
+    // create a four-element tuple
+    // - elements are initialized with default value (0 for fundamental types)
+    std::tuple<std::string, int, int, std::complex<double>> t;
+
+    // create and initialize a tuple explicitly
+    std::tuple<int, double, std::string> t1(41, 6.3, "nico");
+
+    // ``iterate'' over elements
+    std::cout << std::get<0>(t1) << ' ';
+    std::cout << std::get<1>(t1) << ' ';
+    std::cout << std::get<2>(t1) << '\n';
+
+    // create tuple with make_tuple()
+    // - auto declares t2 with type of right-hand side
+    // - thus, type of t2 is tuple
+    auto t2 = std::make_tuple(22, 44, "nico");
+
+    // assign second value in t2 to t1
+    std::get<1>(t1) = std::get<1>(t2);
+
+    // comparison and assignment
+    // - including type conversion from tuple<int, int, const char*>
+    //   to tuple<int, double, std::string>
+    if (t1 < t2) { // compares value for value
+        t1 = t2;   // OK, assigns value for value
+    }
+}
